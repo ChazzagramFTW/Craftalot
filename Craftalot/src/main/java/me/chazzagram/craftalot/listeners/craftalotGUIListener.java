065448f9d4b2d5a4a7a239ddb670d5aab73d2d07
ChazzagramFTW <charlie5charlie@outlook.com>
2023-11-22 13:14:04 +0000

    Pre-build update 23.1122
    - Created inventory to hold Settings
    - The settings ItemStack in /ca gui now directs the user to the Settings inventory when clicked.
    - ItemStacks have been added into the Settings inventory with metadata.

--- Craftalot/src/main/java/me/chazzagram/craftalot/listeners/craftalotGUIListener.java
+++ Craftalot/src/main/java/me/chazzagram/craftalot/listeners/craftalotGUIListener.java
@@ -10,9 +10,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.*;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class craftalotGUIListener implements Listener {
-    Inventory gui = Bukkit.createInventory(null, 36, "§eCraftlist GUI");
+    Inventory guiCraftlist = Bukkit.createInventory(null, 36, "§eCraftlist GUI");
+    Inventory guiSettings = Bukkit.createInventory(null, 36, "§9Settings GUI");
 
     ItemStack itemSelected;
     private final Craftalot plugin;
@@ -34,21 +38,59 @@ public class craftalotGUIListener implements Listener {
                 for (int i = 0; i <= 32; i++){
                     itemSelected = CraftlistConfig.get().getItemStack("craftlist.item" + i);
                     if(itemSelected != null){
-                        gui.setItem(i, itemSelected);
+                        guiCraftlist.setItem(i, itemSelected);
                     }
 
                 }
-                p.openInventory(gui);
+                p.openInventory(guiCraftlist);
 
 
+            } else if (e.getCurrentItem().getType().equals(Material.CRAFTING_TABLE)) {
+                p.sendMessage("§7Loading interface..");
+                p.closeInventory();
+
+                ItemStack[] menuItems = getMenuItems();
+                guiSettings.setItem(10, menuItems[0]);
+                guiSettings.setItem(11, menuItems[1]);
+                guiSettings.setItem(12, menuItems[2]);
+                p.openInventory(guiSettings);
             }
 
         }
 
         if(e.getView().getTitle().equalsIgnoreCase("§eCraftlist GUI")){
-            CraftlistConfig.get().set("craftlist.item" + e.getSlot(), gui.getItem(e.getSlot()));
+            CraftlistConfig.get().set("craftlist.item" + e.getSlot(), guiCraftlist.getItem(e.getSlot()));
             CraftlistConfig.save();
         }
+    }
+
+    private static ItemStack[] getMenuItems() {
+        ItemStack time_limit = new ItemStack(Material.CLOCK);
+        ItemStack day_night = new ItemStack(Material.CLOCK);
+        ItemStack player_visibility = new ItemStack(Material.ENDER_EYE);
+
+        ItemMeta time_limit_meta = time_limit.getItemMeta();
+        time_limit_meta.setDisplayName("§eTime Limit");
+        ArrayList<String> time_limit_lore = new ArrayList<>();
+        time_limit_lore.add("§fSet how long the game will last!");
+        time_limit_meta.setLore(time_limit_lore);
+        time_limit.setItemMeta(time_limit_meta);
+
+        ItemMeta day_night_meta = day_night.getItemMeta();
+        day_night_meta.setDisplayName("§eTime of Day");
+        ArrayList<String> day_night_lore = new ArrayList<>();
+        day_night_lore.add("§fSet the time of day.");
+        day_night_meta.setLore(day_night_lore);
+        day_night.setItemMeta(day_night_meta);
+
+        ItemMeta player_visibility_meta = player_visibility.getItemMeta();
+        player_visibility_meta.setDisplayName("§ePlayer Visibility");
+        ArrayList<String> player_visibility_lore = new ArrayList<>();
+        player_visibility_lore.add("§fDecide if players can see eachother.");
+        player_visibility_meta.setLore(player_visibility_lore);
+        player_visibility.setItemMeta(player_visibility_meta);
+
+        return new ItemStack[]{time_limit, day_night, player_visibility};
     }
 
     @EventHandler
@@ -56,7 +98,7 @@ public class craftalotGUIListener implements Listener {
 
         if(e.getView().getTitle().equalsIgnoreCase("§eCraftlist GUI")){
             for (int i = 0; i <= 32; i++) {
-                CraftlistConfig.get().set("craftlist.item" + i, gui.getItem(i));
+                CraftlistConfig.get().set("craftlist.item" + i, guiCraftlist.getItem(i));
                 CraftlistConfig.save();
             }
         }
@@ -67,7 +109,7 @@ public class craftalotGUIListener implements Listener {
 
         if(e.getView().getTitle().equalsIgnoreCase("§eCraftlist GUI")){
             for (int i = 0; i <= 32; i++) {
-                CraftlistConfig.get().set("craftlist.item" + i, gui.getItem(i));
+                CraftlistConfig.get().set("craftlist.item" + i, guiCraftlist.getItem(i));
                 CraftlistConfig.save();
             }
         }
